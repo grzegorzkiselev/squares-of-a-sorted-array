e3f07797bd017f4c695d47afa331324743b0f003
grzegorzkiselev <egorkakiselev@gmail.com>
2023-12-05 19:11:47 +0300

    Initial

---
+++ baseline+reduce.js
@@ -0,0 +1,22 @@
+var nums = Array
+    .from({length: 1000000}, () => Math.ceil(Math.random() * 20) - 10)
+    .sort((a, b) => a - b);
+
+var sortedSquares = (nums) => {
+    let size = nums.length;
+    let leftPointer = 0;
+    let rightPointer = size - 1;
+
+    return nums.reduce((result, number, index) => {
+        if (Math.abs(nums[leftPointer]) > Math.abs(nums[rightPointer])) {
+            result[size - 1 - index] = nums[leftPointer] * nums[leftPointer];
+            leftPointer++;
+        } else {
+            result[size - 1 - index] = nums[rightPointer] * nums[rightPointer];
+            rightPointer--;
+        }
+        return result;
+    }, [])
+};
+
+sortedSquares(nums)
---
+++ baseline+ternary.js
@@ -0,0 +1,19 @@
+var nums = Array
+    .from({length: 1000000}, () => Math.ceil(Math.random() * 20) - 10)
+    .sort((a, b) => a - b);
+
+var sortedSquares = (nums) => {
+    let leftPointer = 0;
+    let rightPointer = nums.length - 1;
+    let index = rightPointer;
+    let result = [];
+
+    while (index > -1) {
+        result[index--] = Math.abs(nums[leftPointer]) > Math.abs(nums[rightPointer]) 
+        ? nums[leftPointer] * nums[leftPointer++]
+        : nums[rightPointer] * nums[rightPointer--]
+    }
+    return result;
+};
+
+sortedSquares(nums)
